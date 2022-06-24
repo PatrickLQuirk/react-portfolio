d3500a85f74ad83710d2834dc564e87ab6b2c6a0
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-06-23 22:34:41 -0500

    about section and header/nav w/o styles or functionality

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../Navigation';
+
+function Header() {
+    return (
+        <header>
+            <h2>Patrick Quirk</h2>
+            <Navigation></Navigation>
+        </header>
+    )
+};
+
+export default Header
